512c4f85e49fea608e8dafb37c48e048ad859f86
LotuxPunk <clement@vandendaelen.com>
2018-09-19 14:30:42 +0200

    sessions & events

--- src/main/java/com/vandendaelen/autoafkkicker/handlers/AutoKickerServerEventHandler.java
+++ src/main/java/com/vandendaelen/autoafkkicker/handlers/AutoKickerServerEventHandler.java
@@ -1,20 +1,19 @@
 package com.vandendaelen.autoafkkicker.handlers;
 
 import com.vandendaelen.autoafkkicker.AutoAfkKicker;
-import net.minecraft.client.Minecraft;
-import net.minecraft.entity.player.EntityPlayer;
+import com.vandendaelen.autoafkkicker.objects.Session;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.server.MinecraftServer;
-import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraftforge.fml.common.network.FMLNetworkEvent;
 
 import java.util.List;
 
 @Mod.EventBusSubscriber(modid = AutoAfkKicker.MOD_ID)
 public class AutoKickerServerEventHandler {
+
+    public static List<Session> sessions;
 
     @SubscribeEvent
     public static void onServerTickEvent(TickEvent.ServerTickEvent event) {
@@ -22,12 +21,16 @@ public class AutoKickerServerEventHandler {
     }
 
     @SubscribeEvent
-    public static void onPlayerConnectionEvent(FMLNetworkEvent.ServerConnectionFromClientEvent event){
-
+    public static void onPlayerConnectionEvent(PlayerEvent.PlayerLoggedInEvent event){
+        sessions.add(new Session((EntityPlayerMP)event.player,event.player.getPosition(),0));
     }
 
     @SubscribeEvent
-    public static void onPlayerDisconnectionEvent(FMLNetworkEvent.ServerConnectionFromClientEvent event){
-
+    public static void onPlayerDisconnectionEvent(PlayerEvent.PlayerLoggedOutEvent event){
+        for (Session session : sessions){
+            if (session.getPlayer().getName().equals(event.player.getName())){
+                sessions.remove(session);
+            }
+        }
     }
 }
--- src/main/java/com/vandendaelen/autoafkkicker/objects/Session.java
+++ src/main/java/com/vandendaelen/autoafkkicker/objects/Session.java
@@ -7,4 +7,22 @@ public class Session {
     private EntityPlayerMP player;
     private BlockPos pos;
     private long tickAFK;
+
+    public Session(EntityPlayerMP player, BlockPos pos, long tickAFK) {
+        this.player = player;
+        this.pos = pos;
+        this.tickAFK = tickAFK;
+    }
+
+    public EntityPlayerMP getPlayer() {
+        return player;
+    }
+
+    public BlockPos getPos() {
+        return pos;
+    }
+
+    public long getTickAFK() {
+        return tickAFK;
+    }
 }
